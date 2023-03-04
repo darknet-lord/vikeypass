39b71dfda452f9b8423d0e99e4bfb95708862087
darknet-lord <118698244+darknet-lord@users.noreply.github.com>
2023-03-05 01:34:56 +0300

    Add database encryption

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 use std::collections::HashMap;
-use std::fs;
+use std::{env, fs};
+use std::path::Path;
 use std::error::Error;
 use crossterm::{
     event::{self, Event as CEvent, KeyCode},
@@ -19,8 +20,13 @@ use tui::{
     },
     Terminal,
 };
+use serde_json::json;
 use arboard::Clipboard;
 use keyring::{Entry, Result as KeyringResult};
+
+#[macro_use] extern crate magic_crypt;
+
+use magic_crypt::MagicCryptTrait;
 
 
 enum Event<I> {
@@ -28,45 +34,68 @@ enum Event<I> {
     Tick,
 }
 
-struct SelectedItem {
-    background: Color,
-}
-
-impl SelectedItem {
-    fn update_background(&mut self, color: Color) -> () {
-        self.background = color;
-    }
-}
-
-fn set_materpass() -> () {
-    let entry = Entry::new("vikeypass", "current_user").unwrap();
-    entry.set_password("masterkey").unwrap();
+fn set_masterkey(masterkey: String) -> () {
+    /* Add masterkey to the keyring */
+    let user = env::var("USER").unwrap();
+    let entry = Entry::new("vikeypass", &user).unwrap();
+    entry.set_password(&masterkey).unwrap();
 }
 
 fn get_masterkey() -> KeyringResult<String> {
-    let entry = Entry::new("vikeypass", "current_user").unwrap();
+    /* Get masterkey from the keyring */
+    let user = env::var("USER").unwrap();
+    let entry = Entry::new("vikeypass", &user).unwrap();
     entry.get_password()
 }
 
 fn to_clipboard(text: &str) {
+    /* Copy selected password to the system clipboard for some time. */
     let mut clipboard = Clipboard::new().unwrap();
-    clipboard.set_text(text).unwrap();
+    clipboard.set_text(text.clone()).unwrap();
 
     thread::spawn(move || {
+        /* If the password is still in the buffer then erase it. */
         let ten_millis = time::Duration::from_millis(10000);
         thread::sleep(ten_millis);
+        // TODO: Check if the clipboard has changed.
+        // let current_text = clipboard.get_text().unwrap();
         clipboard.set_text("").unwrap();
     });
 }
 
 fn load_database() -> Result<HashMap<String, String>, Box<dyn Error>> {
-    let data = fs::read_to_string(".vikeypass.json")
+    /* Uses password from user's keyring to decrypt the data */
+    let mcrypt = new_magic_crypt!(get_masterkey().unwrap(), 256);
+    let filepath = get_database_filepath();
+    let encrypted_data = fs::read_to_string(filepath)
         .expect("Should have been able to read the file");
-    let map: HashMap<String, String> = serde_json::from_str(&data).unwrap();
+    let decrypted_data = mcrypt.decrypt_base64_to_string(&encrypted_data).unwrap();
+    let map: HashMap<String, String> = serde_json::from_str(&decrypted_data).unwrap();
     Ok(map)
 }
 
+fn save_database(passwords: &HashMap<String, String>) -> () {
+    /* Uses password from user's keyring to encrypt the data */
+    let json_passwords = json!(passwords);
+    let mcrypt = new_magic_crypt!(get_masterkey().unwrap(), 256);
+    let encrypted_data = mcrypt.encrypt_str_to_base64(json_passwords.to_string());
+    let filepath = get_database_filepath();
+    fs::write(filepath, encrypted_data).expect("Unable to write file");
+}
+
+fn get_database_filepath() -> String {
+    /* Default value is `~/.vikeypass.data` */
+    let home_dir = env::var("HOME").unwrap();
+    let file_env = std::env::var("VIKEYPASS_FILE");
+    match  file_env {
+        Ok(p) => p,
+        Err(_) => String::from(Path::new(&home_dir).join(".vikeypass.data").to_str().unwrap()),
+    }
+}
+
 fn main() -> Result<(), Box<dyn std::error::Error>> {
+    // TODO: Add to setup.
+    // set_masterkey(String::from("masterkey"));
     enable_raw_mode().expect("can run in raw mode");
 
     let (tx, rx) = mpsc::channel();
@@ -93,7 +122,6 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     });
 
     let mut footer_message = "";
-
     let menu_titles = vec!["Home", "Add", "Edit", "Delete", "Quit"];
     let mut passwords = load_database().unwrap();
     let mut selected_idx = 0;
@@ -120,8 +148,10 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
 
             let items: Vec<ListItem> = passwords.keys().enumerate().map(|(idx, keyname)| {
                 match idx {
-                    i if i == selected_idx => ListItem::new(keyname.clone()).style(Style::default().bg(Color::Yellow)),
-                    _ => ListItem::new(keyname.clone()).style(Style::default().bg(Color::Black)),
+                    i if i == selected_idx => ListItem::new(keyname.clone())
+                        .style(Style::default().bg(Color::Yellow)),
+                    _ => ListItem::new(keyname.clone())
+                        .style(Style::default().bg(Color::Black)),
                 }
             }).collect();
 
@@ -196,12 +226,13 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
                     let key = passwords.keys().nth(selected_idx as usize).unwrap();
                     passwords.remove(&key.clone()).unwrap();
                     footer_message = "Entry has been destroyed";
+                    // TODO: Save the changes.
+                    // save_database(&passwords);
                 },
                 _ => ()
             },
             Event::Tick => {}
         }
-
     }
     Ok(())
 } 
